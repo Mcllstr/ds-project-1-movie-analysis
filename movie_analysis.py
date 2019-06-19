15ea6ae698dc274f88d82b7b2f8ddc5725f8604a
Sean Carver <seancarverphd@gmail.com>
2019-06-19 14:47:18 -0400

    Pruned by sample size

--- movie_analysis.py
+++ movie_analysis.py
@@ -83,8 +83,12 @@ for index, element in enumerate(the_df['roi']):
 
 
 
-
-
-
-
-
+        
+median_genres = the_df.groupby('genres').roi.median().sort_values(ascending = False)
+count_genres = the_df.groupby('genres').roi.count().sort_values(ascending = False)
+count_df = pd.DataFrame(count_genres)
+median_df = pd.DataFrame(median_genres)
+joined_genre_df = median_df.join(count_df, how='inner', lsuffix='median', rsuffix='count')
+joined_genre_df.sort_values(by='roicount', ascending=False)
+genre_keepers = joined_genre_df[joined_genre_df['roicount'] >= 10]
+print(genre_keepers.head())
